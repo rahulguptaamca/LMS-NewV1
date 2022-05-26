6b875ff66098d1085e174a72c825cdd452626461
Rahul Gupta <rahugupta@tsys.com>
2022-05-26 07:18:08 +0530

    new changes commit

---
+++ src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
@@ -0,0 +1,13 @@
+package com.rahul.learning.LibraryManagementSystemV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.learning.LibraryManagementSystemV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
