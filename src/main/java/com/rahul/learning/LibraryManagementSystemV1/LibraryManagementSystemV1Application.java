8814d8fc05d63181dfdc72a8dfd9889ded244ead
rahulguptaamca <53555505+rahulguptaamca@users.noreply.github.com>
2022-06-01 12:25:23 +0530

    Changes saved from remote server on 1Jun22-1224PM

--- src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
+++ src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
@@ -10,6 +10,7 @@ public class LibraryManagementSystemV1Application {
 		System.out.println("Hello main program started with one sysout changes done from remote server");
 		System.out.println("Made changes add one line locally on main branch at 31May22-7-54PM");
 		System.out.println("Made changes add one line from remote server on main branch at 31May22-7-55PM");
+		System.out.println("Made changes add one line remotely on main branch at 1June22-12-21PM");
 		SpringApplication.run(LibraryManagementSystemV1Application.class, args); 
 	}
 
