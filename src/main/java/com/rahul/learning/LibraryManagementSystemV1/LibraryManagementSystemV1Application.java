772a3cbde2dfc2915cb7b4df892ab464179cd16d
Rahul Gupta <rahugupta@tsys.com>
2022-06-08 16:20:23 +0530

    new changes at8June22-419PM

--- src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
+++ src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
@@ -7,14 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryManagementSystemV1Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello main program started with one sysout changes done from remote server");
-		System.out.println("Made changes add one line locally on main branch at 31May22-7-54PM");
-		System.out.println("Made changes add one line from remote server on main branch at 31May22-7-55PM");
-		System.out.println("Made changes add one line remotely on main branch at 1June22-12-21PM");
-		System.out.println("Made changes add one line remotely on main branch at 1June22-12-55PM");
 		
-		System.out.println("Made changes add one line remotely on main branch at 2June22-06-00AM");
 		SpringApplication.run(LibraryManagementSystemV1Application.class, args); 
+	}
+	
+	public void method1() {
+		System.out.println("added new method1");
 	}
 
 }
