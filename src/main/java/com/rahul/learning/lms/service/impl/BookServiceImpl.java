3fc2b9ff7218d700210335369321423894bec4ce
Rahul Gupta <rahugupta@tsys.com>
2022-08-07 17:54:20 +0530

    BookServiceImpl changes commit on 7Aug22At0554PM

--- src/main/java/com/rahul/learning/lms/service/impl/BookServiceImpl.java
+++ src/main/java/com/rahul/learning/lms/service/impl/BookServiceImpl.java
@@ -5,5 +5,10 @@ public class BookServiceImpl {
 	public BookServiceImpl() {
 		//default constructor.
 	}
+	
+	public BookServiceImpl(String s1) {
+		//one Parametrized constructor.
+		System.out.println("Print value of s1"+s1);
+	}
 
 }
