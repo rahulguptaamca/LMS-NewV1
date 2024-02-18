112fc26556b3b83cac1d804c57d995932d550ad9
Rahul Gupta <rahulguptaamca@gmail.com>
2024-02-19 01:48:10 +0530

    Revert "commiting changes on 19Feb24_1_36AM"
    This reverts commit 47f7a3db6e60a5d0a92e8109e1d33b74a391931e.

--- src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
+++ src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
@@ -8,7 +8,6 @@ public class LibraryManagementSystemV1Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello main program started with one sysout changes done from remote server");
-		System.out.println("Made changes add one line locally on main branch at 19feb24-1_34AM");
 		SpringApplication.run(LibraryManagementSystemV1Application.class, args); 
 	}
 
