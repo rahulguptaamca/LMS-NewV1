2874e7d6717f57906c002ed0f879e41e2898532a
Rahul Gupta <rahulguptaamca@gmail.com>
2024-02-26 23:27:30 +0530

    changes commit  from local server at 26Feb2024At11:27PM

--- src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
+++ src/main/java/com/rahul/learning/LibraryManagementSystemV1/LibraryManagementSystemV1Application.java
@@ -9,6 +9,8 @@ public class LibraryManagementSystemV1Application {
 	public static void main(String[] args) {
 		System.out.println("Hello main program started with one sysout changes done from remote server");
 		SpringApplication.run(LibraryManagementSystemV1Application.class, args); 
+		System.out.println("*****************************************");
+		System.out.println("One sysout changes added from local server at 26Feb2024At11:28PM");
 	}
 
 }
